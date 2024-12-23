3cd147579caa4829d1fe023d429c2a1e3e3fe2c8
ChoiWheatley <chltmdgus604@naver.com>
2024-12-23 18:58:36 +0900

    2024-12-23 manual commit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,22 @@
 return {
+  "nvim-lua/plenary.nvim",
+  "nvchad/volt",
+
+  {
+    "nvchad/ui",
+    config = function()
+      require "nvchad"
+    end,
+  },
+
+  {
+    "nvchad/base46",
+    lazy = true,
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
+
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
@@ -13,13 +31,34 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "dstein64/vim-startuptime",
+    -- lazy-load on a command
+    cmd = "StartupTime",
+    -- init is called during startup. Configuration for vim plugins typically should be set in an init function
+    init = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
+
+  {
+    "mrcjkb/haskell-tools.nvim",
+    version = "^4", -- Recommended
+    lazy = false,   -- This plugin is already lazy
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css"
+      },
+    },
+  },
+
+  {
+    "wakatime/vim-wakatime",
+    lazy = false,
+  },
 }
